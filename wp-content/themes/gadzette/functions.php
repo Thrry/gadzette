3252168c54038cad82d64732516bcd7d98530a2b
Thrry <thierryjouannic@gmail.com>
2026-07-04 01:51:00 +0200

    Implement newspaper homepage design

--- wp-content/themes/gadzette/functions.php
+++ wp-content/themes/gadzette/functions.php
@@ -2,9 +2,16 @@
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
+        'gadzette-tokens',
+        get_template_directory_uri() . '/tokens.css',
+        [],
+        wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_style(
         'gadzette-theme',
         get_stylesheet_uri(),
-        [],
+        ['gadzette-tokens'],
         wp_get_theme()->get('Version')
     );
 });
